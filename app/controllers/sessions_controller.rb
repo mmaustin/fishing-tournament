7ccf8159b10229da267b814ab529127c5d413f59
McCray <tubach75@hotmail.com>
2021-05-10 16:32:38 -0400

    commenced creating the login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,16 @@ class SessionsController < ApplicationController
     end
 
     def create
+        angler = Angler.find_by(name: params[:name])
+        if angler && angler.authenticate([params[:password]])
+            session[:user_id] = angler.id 
+        else
+            render :new
+        end
     end
 
     def destroy
+        session.delete :user_id
     end
 
 end
